8ac9b472a1abf4750decafbc2cf1db725d90fe38
nikvettese <76953721+nikvettese@users.noreply.github.com>
2021-01-21 11:05:10 -0500

    Create Item.java

---
+++ src/Item.java
@@ -0,0 +1,28 @@
+public class Item {
+    private String name;
+    private String value;
+
+    public Item() {
+    }
+
+    public Item(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
